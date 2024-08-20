c7d632527e7cac28670232084112e3b27a960378
ADGR641 <adgr641@gmail.com>
2024-08-21 00:13:22 +0800

    21/08/2024

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,46 @@
 import React from "react";
 import "../styles/NavBar.css";
 import ReorderIcon from "@mui/icons-material/Reorder";
-import { Reorder } from "@mui/icons-material";
+import { useState } from "react";
 
 function NavBar() {
+  const [showNavBar, activeNavBar] = useState(false);
+  //['current State',' Modify State']
+
+  const navClick = () => {
+    activeNavBar(!showNavBar);
+  };
+
   return (
-    <div className="navBar">
-      <div className="leftSide">
-        <a>Finance Flow</a>
+    <>
+      <div className="navBar">
+        <div className="leftSide">
+          <a>Finance Flow</a>
+        </div>
+        <button className="navIcon" onClick={navClick}>
+          <ReorderIcon />
+        </button>
+        <div className="rightSide">
+          <a>Dashboard</a>
+          <a>Transaction</a>
+          <a>Categories</a>
+          <a>Budget</a>
+          <a>Report</a>
+          <a>Goals</a>
+          <a>Profile</a>
+        </div>
       </div>
-      <div className="rightSide">
+      <div className="hiddenRightSide" id={showNavBar ? "show" : "hide"}>
+        {/* if true then set to show if false then set to hide  */}
         <a>Dashboard</a>
-
         <a>Transaction</a>
-
         <a>Categories</a>
-
         <a>Budget</a>
-
         <a>Report</a>
-
         <a>Goals</a>
-
         <a>Profile</a>
       </div>
-      <div className="navIcon">
-        <ReorderIcon />
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
-import NavBar from "../components/NavBar.js";
 import "../styles/Home.css";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="showAmount">Available Amount</div>
+      <div>$1000</div>
+    </div>
+  );
 }
 
 export default Home;
