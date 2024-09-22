d0310d50ad465688f472b1be0e12002e2d7c15a3
ADGR641 <adgr641@gmail.com>
2024-09-22 15:38:33 +0800

    22/9

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,76 +6,114 @@ import LineChart from "../components/LineChart.js";
 function Dashbaord() {
   return (
     <>
-      <div className="dashBoardBody">
-        <div className="introduction">
-          <Person2Icon />
-          <div className="greetName">
-            Good Morning, Andrian
-            <div className="introductionFiller">
-              Let's manage your wallet finance
+      <body>
+        <div className="dashBoardBody">
+          <div className="introduction">
+            <Person2Icon />
+            <div className="greetName">
+              Good Morning, Andrian
+              <div className="introductionFiller">
+                Let's manage your wallet finance
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="totalStats">
-          <div className="totalRevenue">
-            <div className="totalStatsTopSide">
-              <div className="totalStatsPercent">+10%</div>
-              <div className="totalStatschart"></div>
+          <div className="totalStats">
+            <div className="totalRevenue">
+              <div className="totalStatsTopSide">
+                <div className="totalStatsPercent">+10%</div>
+                <div className="totalStatschart"></div>
+              </div>
+              <div className="totalStatsBottomSide">
+                <div className="totalStatsTitle">Total Revenue</div>
+                <div className="totalStatsAmount">$100,000</div>
+              </div>
             </div>
-            <div className="totalStatsBottomSide">
-              <div className="totalStatsTitle">Total Revenue</div>
-              <div className="totalStatsAmount">$100,000</div>
+            <div className="totalExpenses">
+              <div className="totalStatsTopSide">
+                <div className="totalStatsPercent">-8.8%</div>
+                <div className="totalStatschart">Picture</div>
+              </div>
+              <div className="totalStatsBottomSide">
+                <div className="totalStatsTitle">Total Expenses</div>
+                <div className="totalStatsAmount">$80,000</div>
+              </div>
+            </div>
+            <div className="totalProfit">
+              <div className="totalStatsTopSide">
+                <div className="totalStatsPercent">-21.8%</div>
+                <div className="totalStatschart">Picture</div>
+              </div>
+              <div className="totalStatsBottomSide">
+                <div className="totalStatsTitle">Total Profit</div>
+                <div className="totalStatsAmount">$21,600</div>
+              </div>
             </div>
           </div>
-          <div className="totalExpenses">
-            <div className="totalStatsTopSide">
-              <div className="totalStatsPercent">-8.8%</div>
-              <div className="totalStatschart">Picture</div>
-            </div>
-            <div className="totalStatsBottomSide">
-              <div className="totalStatsTitle">Total Expenses</div>
-              <div className="totalStatsAmount">$80,000</div>
-            </div>
-          </div>
-          <div className="totalProfit">
-            <div className="totalStatsTopSide">
-              <div className="totalStatsPercent">-21.8%</div>
-              <div className="totalStatschart">Picture</div>
-            </div>
-            <div className="totalStatsBottomSide">
-              <div className="totalStatsTitle">Total Profit</div>
-              <div className="totalStatsAmount">$21,600</div>
-            </div>
-          </div>
-        </div>
 
-        <div className="analysisSection">
-          <div className="analysisChart">
-            <LineChart />
-          </div>
-        </div>
-
-        <div className="transactionHistorySection">
-          <div className="transactionHistoryTopSide">
-            <div>Transaction History</div>
-            <div className="transactionHistorySorting">
-              <div className="transactionHistoryNewest">Newest</div>
-              <div className="transactionHistoryOldests">Oldest</div>
+          <div className="analysisSection">
+            <div className="analysisChart">
+              <LineChart />
             </div>
           </div>
-          <div className="transactionHistoryBottomSide">
-            <div>Transaction</div>
-            <div>Type</div>
-            <div>Amount</div>
-            <div>Date</div>
-            <div>Status</div>
-          </div>
-          <div>
-            <ul></ul>
+
+          <div className="transactionHistorySection">
+            <div className="transactionHistoryTopSide">
+              <div>Transaction History</div>
+              <div className="transactionHistorySorting">
+                <div className="transactionHistoryNewest">Newest</div>
+                <div className="transactionHistoryOldests">Oldest</div>
+              </div>
+            </div>
+            <div className="transactionHistoryBottomSide">
+              <div>
+                <div className="transactionHistorytitle">Transaction</div>
+                <ul>
+                  <li>Upwork</li>
+                  <li>Netflix</li>
+                  <li>Spotify</li>
+                </ul>
+              </div>
+
+              <div>
+                <div className="transactionHistorytitle">Type</div>
+                <ul>
+                  <li>Freelancing</li>
+                  <li>Entertainment</li>
+                  <li>Subscription</li>
+                </ul>
+              </div>
+
+              <div>
+                <div className="transactionHistorytitle">Amount</div>
+                <ul>
+                  <li>$150</li>
+                  <li>$60</li>
+                  <li>$10</li>
+                </ul>
+              </div>
+
+              <div>
+                <div className="transactionHistorytitle">Date</div>
+                <ul>
+                  <li>14 Aug, 2023</li>
+                  <li>10 Aug, 2023</li>
+                  <li>29 July, 2023</li>
+                </ul>
+              </div>
+
+              <div>
+                <div className="transactionHistorytitle">Status</div>
+                <ul>
+                  <li>Credited</li>
+                  <li>Debited</li>
+                  <li>Debited</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </body>
     </>
   );
 }
